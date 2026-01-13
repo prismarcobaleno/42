c57b69c17153f1d93b6891540c910e13ff9d62d3
prismarcobaleno <you@example.com>
2026-01-13 19:15:28 +0100

    modified:   ft_substr.c

---
+++ LIBBEFFETTI/ft_split.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+
+	if (!(split = ft_alloc_split(s, c)))
+		return (NULL);
+	if (!ft_split_by_char(split, s, c))
+		return (NULL);
+	return (split);
+}
