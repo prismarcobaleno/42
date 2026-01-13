5478b8418d71af7222cd67f0ffc00f94b2660805
prismarcobaleno <you@example.com>
2026-01-13 13:52:38 +0100

    ft_strtrim.c

--- LIBBEFFETTI/ft_strtrim.c
+++ LIBBEFFETTI/ft_strtrim.c
@@ -3,21 +3,40 @@
 // TOGLI!!!!!!!!!!!!!!!!!!!!!!!
 #include "libft.h"
 
+
+static	int rientra(char c, char const *qui)
+{
+	unsigned long i;
+	i = 0;
+
+	while (qui[i++])
+	{
+		if (qui[i] == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	long int	i;
-	long int	j;
+//	long int	iniz;
+//	long int	fine;
 	char		*stri;
 
 	stri = ft_strdup(s1);
 	i = 0;
-	j = 0;
-	while (set[j++])
+	while (s1[i++] && rientra(stri[i], set))
 	{
-		while (s1[i++] == set[j])
-		{
-			stri[i] = '\0';
-		}
+		stri[i] = '\0';
+	}
+	//metti variabili alla fine della parte iniziale da tagliare e all'inizio della parte finale
+	i = ft_strlen(s1);
+	while (s1[i--] && rientra(stri[i], set))
+	{
+		stri[i] = '\0';
 	}
 	return (stri);
 }
